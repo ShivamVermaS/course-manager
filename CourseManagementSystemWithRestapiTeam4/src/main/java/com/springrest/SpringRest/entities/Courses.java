60eee84fdf627194bf0fd5c26f7a5d1ac414c327
ShivamVermaS <shivam.verma@walkingtree.tech>
2022-06-28 16:42:23 +0530

    update

--- CourseManagementSystemWithRestapiTeam4/src/main/java/com/springrest/SpringRest/entities/Courses.java
+++ CourseManagementSystemWithRestapiTeam4/src/main/java/com/springrest/SpringRest/entities/Courses.java
@@ -13,92 +13,105 @@ public class Courses {
 	private long fee;
 	private String Category;
 	private String image;
+	String discription;
 	
 	
-	public Courses(long id, String title, String duration, long fee, String category, String image) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.duration = duration;
-		this.fee = fee;
-		Category = category;
-		this.image = image;
-	}
-
-
-	public Courses()
+		public Courses()
 	{
 		super();
 		
 	}
+		
+	
+public Courses(long id, String title, String duration, long fee, String category, String image,
+				String discription) {
+			super();
+			this.id = id;
+			this.title = title;
+			this.duration = duration;
+			this.fee = fee;
+			Category = category;
+			this.image = image;
+			this.discription = discription;
+		}
+
 
 
 	public long getId() {
-		return id;
-	}
+	return id;
+}
 
 
-	public void setId(long id) {
-		this.id = id;
-	}
+public void setId(long id) {
+	this.id = id;
+}
 
 
-	public String getTitle() {
-		return title;
-	}
+public String getTitle() {
+	return title;
+}
 
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+public void setTitle(String title) {
+	this.title = title;
+}
 
 
-	public String getDuration() {
-		return duration;
-	}
+public String getDuration() {
+	return duration;
+}
 
 
-	public void setDuration(String duration) {
-		this.duration = duration;
-	}
+public void setDuration(String duration) {
+	this.duration = duration;
+}
 
 
-	public long getFee() {
-		return fee;
-	}
+public long getFee() {
+	return fee;
+}
 
 
-	public void setFee(long fee) {
-		this.fee = fee;
-	}
+public void setFee(long fee) {
+	this.fee = fee;
+}
 
 
-	public String getCategory() {
-		return Category;
-	}
+public String getCategory() {
+	return Category;
+}
 
 
-	public void setCategory(String category) {
-		Category = category;
-	}
+public void setCategory(String category) {
+	Category = category;
+}
 
 
-	public String getImage() {
-		return image;
-	}
+public String getImage() {
+	return image;
+}
 
 
-	public void setImage(String image) {
-		this.image = image;
-	}
+public void setImage(String image) {
+	this.image = image;
+}
 
 
-	@Override
-	public String toString() {
-		return "Courses [id=" + id + ", title=" + title + ", duration=" + duration + ", fee=" + fee + ", Category="
-				+ Category + ", image=" + image + "]";
-	}
-	
+public String getDiscription() {
+	return discription;
+}
+
+
+public void setDiscription(String discription) {
+	this.discription = discription;
+}
+
+
+@Override
+public String toString() {
+	return "Courses [id=" + id + ", title=" + title + ", duration=" + duration + ", fee=" + fee + ", Category="
+			+ Category + ", image=" + image + ", discription=" + discription + "]";
+}
 
 	
 }
